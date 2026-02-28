b2a902e26f81dcc1ecea4f26c33b8833e87d13d8
caglayenici <70663017+caglayenici@users.noreply.github.com>
2026-02-28 15:46:35 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'whoami-pwa-v1';
+const ASSETS = [
+  './index.html',
+  './play.html',
+  './manifest.webmanifest',
+  './sw.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
